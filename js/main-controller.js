1ce50d683a32791442370ae1763ec8f90fcb3cc8
Evgey <evgeny12712@gmail.com>
2021-10-27 14:31:50 +0300

    main design complete

--- js/main-controller.js
+++ js/main-controller.js
@@ -1,1 +1,12 @@
 'use strict';
+renderImages();
+
+function renderImages() {
+    var images = getImages();
+    var elImagesContainer = document.querySelector('.images-container');
+    var strHtmls = images.map(image => {
+        return `<img src="images/gallery/${image}.jpg" />`
+    })
+    console.log('strHtmls', strHtmls);
+    document.querySelector('.images-container').innerHTML = strHtmls.join('');
+}
